c127429d19c5a8fe3ac7419811f143ca20c202ac
achawin <achawin1998s_@hotmail.com>
2024-06-07 07:19:27 +0700

    a-shop

--- src/app/cart/api/checkout-sessions/route.js
+++ src/app/cart/api/checkout-sessions/route.js
@@ -1,91 +1,73 @@
 import { stripe } from "src/app/utils/stripe";
-import { NextResponse } from "next/server";
  
-async function lineItem(products, cartDetails) {  
-  const lineItems = [];
-
-  for (const [productId, product] of Object.entries(cartDetails)) {
-    const productData = products.find((p) => p.id === productId);  
-    if (productData) {
-      lineItems.push({
-        price_data: {
-          currency: productData.currency,
-          product_data: {
-            name: productData.name,
-            images: [productData.image],
+async function lineItem(products, cartDetails) {  // สร้างข้อมูลที่่เอาไว้ในการสร้าง session กำหนด parameter 2 ตัว เพื่อเอาไว้รับค่า
+    const lineItems = [];
+  
+    for (const [productId, product] of Object.entries(cartDetails)) {
+      const productData = products.find((p) => p.id === productId);  // ทำการ เช็คค่า id 
+      if (productData) {
+        lineItems.push({
+          price_data: {
+            currency: productData.currency,
+            product_data: {
+              name: productData.name,
+              images: [productData.image],
+            },
+            unit_amount_decimal: productData.price,
           },
-          unit_amount: productData.price,
-        },
-        quantity: product.quantity,
-      });
+          quantity: product.quantity,
+        });
+      }
     }
+    return lineItems;
   }
-  return lineItems;
-}
-
-export async function POST(req) {
-  try {
-    const cartDetails = await req.json();
-    const inventory = await stripe.products.list({ expand: ['data.default_price'] });
-    const products = inventory.data.map((product) => {
-      const price = product.default_price;
-      return {
-        currency: price.currency,
-        id: product.id,
-        name: product.name,
-        price: price.unit_amount,
-        image: product.images[0],
-      };
-    });
-    const lineItems = await lineItem(products, cartDetails);
-    const session = await stripe.checkout.sessions.create({
-      mode: 'payment',
-      payment_method_types: ['card'],
-      line_items: lineItems,
-      success_url: `${req.headers.get('origin')}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${req.headers.get('origin')}/cart`,
-    });
-
-    const response = new NextResponse(JSON.stringify(session), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-
-    return response;
-  } catch (error) {
-    console.log(error);
-    if (error.type === 'StripeInvalidRequestError') {
-      return new NextResponse(JSON.stringify({ error: error.message }), {
-        status: 400,
+  
+ 
+export async function POST(req, res) {
+    try {
+      const cartDetails = await req.json(); // รับค่าข้อมูลสินค้าต่าง ๆ จากหน้า cart 
+      const inventory = await stripe.products.list({ expand: ['data.default_price'] }); // นำตัว stripe มา map ข้อมูลเพื่อโชว์ข้อมูลสินค้าต่าง ๆ เหมือนหน้าแรก
+      const products = inventory.data.map((product) => {
+        const price = product.default_price;
+        return {
+          currency: price.currency,
+          id: product.id,
+          name: product.name,
+          price: price.unit_amount,
+          image: product.images[0],
+        };
+      });
+      const lineItems = await lineItem(products, cartDetails); // เรียกใช้ฟังชันที่สร้าง และใส่ค่า product , cartDetails เพื่อเช็คว่า ค่าที่ได้รับมัน match กันมั้ย และเก็บค่าไว้ในตัวแปร
+      const session = await stripe.checkout.sessions.create({ // กำหนด session ต่าง ๆ และสั่ง redirect ไปหน้า payment 
+        mode: 'payment',
+        payment_method_types: ['card'], // ใช้การจ่ายเป็นบัตร
+        line_items: lineItems, // เรียกใช้ตรงนี้ ตัวนนี้คือข้อมูลต่าง ๆ ที่ได้รับกลับมาจาก lineItems
+        success_url: `${req.headers.get('origin')}/success?session_id={CHECKOUT_SESSION_ID}`, // ได้รับ session id มาจาก checkout session create ถูก return มาตรงนี้
+        cancel_url: `${req.headers.get('origin')}/cart`, // cancle กลับมาหน้า cart ที่เดิม ถ้าสำเร็จ redirect ไปหน้า payment
+      }); 
+      
+      return new Response(JSON.stringify(session), { // return session ออกมา หน้า client
+        status: 200,
         headers: {
           'Content-Type': 'application/json',
         },
       });
-    } else {
-      return new NextResponse(JSON.stringify({ statusCode: 500, message: error.message }), {
-        status: 500,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+    } catch (error) {
+      console.log(error);
+      if (error.type === 'StripeInvalidRequestError') {
+        return new Response(JSON.stringify({ error: error.message }), {
+          status: 400,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+      } else {
+        return new Response(JSON.stringify({ statusCode: 500, message: error.message }), {
+          status: 500,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+      }
     }
   }
-}
-
-export async function OPTIONS() {
-  const response = new NextResponse(null, {
-    status: 204,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    },
-  });
-  return response;
-}
