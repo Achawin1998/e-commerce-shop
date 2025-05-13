de073bb20009358592189caa96eeafb9d2e3318b
achawin <achawin1998s_@hotmail.com>
2025-05-13 15:47:06 +0700

    update-nextjs-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,30 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: ['epic-projects.nyc3.digitaloceanspaces.com']
+//     },
+//     async rewrites() {
+//         return [
+//             {
+//                 source: '/api/:path*',
+//                 destination: 'https://e-commerce-shop-livid.vercel.app/api/:path*',
+//             },
+//         ];
+//     }
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['epic-projects.nyc3.digitaloceanspaces.com']
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'epic-projects.nyc3.digitaloceanspaces.com',
+                pathname: '/**',
+            },
+        ],
     },
     async rewrites() {
         return [
@@ -10,7 +33,7 @@ const nextConfig = {
                 destination: 'https://e-commerce-shop-livid.vercel.app/api/:path*',
             },
         ];
-    }
+    },
 };
 
 export default nextConfig;
